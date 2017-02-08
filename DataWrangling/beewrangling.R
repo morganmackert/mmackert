02d2df3c4e8d45443ab6b20c6db543f2b8c28d45
Morgan <mmackert@iastate.edu>
2017-02-08 13:09:54 -0600

    Set up

---
+++ DataWrangling/beewrangling.R
@@ -0,0 +1,45 @@
+#####################################
+############### BEES ################
+#####################################
+
+#Set working directory
+setwd("~/ISU/Project/mmackert")
+
+#Read in data files
+bees <- read.csv("Data/bees/working/2016Bees.csv", header = T)
+
+#View data
+View(bees)
+
+#Load libraries
+library(lubridate)
+
+#Fix dates in "bees"
+bees$Date <- mdy(bees$Date)
+
+#Eliminate last row (totals)
+bees <- bees[-c(124),]
+
+#View data to check date/data changes
+View(bees)
+
+
+#Subset "bees" to eliminate extraneous data; only need "Date", "Site", and "Bees".
+simplebees <- subset(bees[,c(1:2,4)])
+
+#View new data frame
+View(simplebees)
+#Looks good
+
+#Not worrying about trap effects just yet, so combine by date and site.
+#HOW?
+
+
+
+
+
+
+
+
+
+
