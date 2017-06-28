24f0b69b57e873344f2d9d106949ab7c9c57c7c0
Morgan <mmackert@iastate.edu>
2017-06-28 11:13:00 -0500

    Update

--- AnalysisScripts/iNEXT.R
+++ AnalysisScripts/iNEXT.R
@@ -26,10 +26,10 @@ setwd("~/ISU/Project/mmackert/Data")
 PlunkettBowman <- read.csv("bees/working/PlunkettBowman.csv")
 #Data must be as a dataframe
 PlunkettBowman <- as.data.frame(PlunkettBowman)
-#Need to change the rownames to the species rather than numbers
+#Change the rownames to the species rather than numbers
 PlunkettBowman2 <- PlunkettBowman[,-1]
 rownames(PlunkettBowman2) <- PlunkettBowman[,1]
-#Check the dataframe out to make sure all is well
+#Check the dataframe to make sure all is well
 str(PlunkettBowman2)
 
 #RUN THE TEST
@@ -41,3 +41,17 @@ ggiNEXT(out4, type = 1, facet.var = "order")
 
 #Now try it with the whole previous dataset!
 #Read in the data
+iNEXTAbundance <- read.csv("bees/working/iNEXTAbundance.csv")
+#Data must be as a dataframe if not brought in as dataframe.
+iNEXTAbundance <- as.data.frame(iNEXTAbundance)
+#Change the rownames to the species rather than numbers
+iNEXTAbundance2 <- iNEXTAbundance[,-1]
+rownames(iNEXTAbundance2) <- iNEXTAbundance[,1]
+#Check the dataframe to make sure all is well
+str(iNEXTAbundance2)
+
+#RUN THE TEST
+iNEXTResult <- iNEXT(iNEXTAbundance2, q = c(0, 1, 2), datatype = "abundance")
+
+#Make pretty graphs
+ggiNEXT(iNEXTResult, type = 1, facet.var = "site")
