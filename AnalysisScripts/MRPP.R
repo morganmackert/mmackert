73c76176f52159a151212d1b6e0461b1418847d7
morganmackert <mmackert@iastate.edu>
2017-12-31 15:21:51 -0600

    MRPP updates

--- AnalysisScripts/MRPP.R
+++ AnalysisScripts/MRPP.R
@@ -10,10 +10,11 @@ setwd("~/ISU/Project/Data")
 #Load libraries
 library(lubridate)
 library(dplyr)
+library(tidyr)
 library(vegan)
 
 #Read in data
-Quadrats <- read.csv("Plants/Quadrats.csv", header = T, na.strings = c("", "NA"))
+Quadrats <- read.csv("Plants/Quadrats.csv", header = T, na.strings = c("", "NA"), stringsAsFactors = TRUE)
 #Date = Date of sample
 #Year = Year of the study; 1 = 2014, 2 = 2015, 3 = 2016, 4 = 2017
 #Sample; 1 = Early May, 2 = Late May, 3 = June, 4 = July, 5 = August
@@ -37,6 +38,7 @@ names(Quadrats)[names(Quadrats) == "Species.in.Strip...Not.in.Quadrats"] <- "Str
 Quadrats123GR <- Quadrats %>%
   filter(Year <= 3) %>%
   filter(Site == "Greving")
+
 Quadrats123PE <- Quadrats %>%
   filter(Year <= 3) %>%
   filter(Site == "Peckumn")
@@ -50,49 +52,53 @@ numbsquadrats123PE <- Quadrats123PE %>%
   filter(!is.na(Species)) %>%
   summarise(TotalBS = length(unique(Species)))
 
-#Determine what the blooming species are
+#Determine what the blooming species are for Greving and Peckumn by date, while retaining site names
 bsquadrats123GR <- Quadrats123GR %>%
+  group_by(Date, Site) %>%
   filter(!is.na(Species)) %>%
   count(Species)
 
 bsquadrats123PE <- Quadrats123PE %>%
+  group_by(Date, Site) %>%
   filter(!is.na(Species)) %>%
   count(Species)
 
+#Join Greving and Peckumn data frames
+bsquadrats123GRPE <- full_join(bsquadrats123GR, bsquadrats123PE, by = c("Species", "Site", "Date", "n"))
+
+#Reformat from long to wide
+bsquadrats123GRPEwide <- spread(bsquadrats123GRPE, Species, n)
+
+#Fill NAs with 0
+bsquadrats123GRPEwide[is.na(bsquadrats123GRPEwide)] <- 0
+
+#Export data file for Mary
+write.csv(bsquadrats123GRPEwide, file = "C:/Users/morga/Documents/ISU/Project/mmackert/Graphs/MRPP/Greving and Peckumn Blooming Species")
+
+#Remove "Date" and "Site" columns from bsquadrats123GRPEwide data frame
+bsquadrats123GRPEwide <- bsquadrats123GRPEwide[!names(bsquadrats123GRPEwide) %in% c("Date", "Site")]
+
 #Calculate average vegetation coverage
 averageveg123GR <- Quadrats123GR %>%
+  group_by(Date, Site) %>%
   filter(!is.na(Species)) %>%
   summarise(AverageVeg = mean(Cover))
 
 averageveg123PE <- Quadrats123PE %>%
+  group_by(Date, Site) %>%
   filter(!is.na(Species)) %>%
   summarise(AverageVeg = mean(Cover))
 
+#Join Greving and Peckumn data frames
+averageveg123GRPE <- full_join(averageveg123GR, averageveg123PE, by = c("Site", "Date", "AverageVeg"))
+
+#Export data file for Mary
+write.csv(averageveg123GRPE, file = "C:/Users/morga/Documents/ISU/Project/mmackert/Graphs/MRPP/Greving and Peckumn Average Vegetation Coverage")
+
+#Convert to data.frames
+averageveg123GRPE <- as.data.frame(averageveg123GRPE)
+bsquadrats123GRPEwide <- as.data.frame(bsquadrats123GRPEwide)
+
 #MRPP analysis
-GRPE.MRPP <- mrpp(bsquadrats123GR, bsquadrats123PE)
-
-
-data(dune)
-data(dune.env)
-dune.mrpp <- mrpp(dune, dune.env$Management)
-dune.mrpp
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+GRPE.MRPP <- mrpp(bsquadrats123GRPEwide, averageveg123GRPE$Site, distance = "bray")
+GRPE.MRPP
