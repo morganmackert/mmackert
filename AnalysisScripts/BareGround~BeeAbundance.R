9ae9f70b7a0376bd1e25e4811b2b975e5323ca86
mmackert <mmackert@iastate.edu>
2019-02-21 19:11:49 -0600

    Multipanel graph

--- AnalysisScripts/BareGround~BeeAbundance.R
+++ AnalysisScripts/BareGround~BeeAbundance.R
@@ -77,6 +77,7 @@ bareground.etrapbees <- full_join(etrapbees, avg.bareground, by = c("Date", "Sit
 bareground.etrapbees <- bareground.etrapbees %>%
   na.omit(number.etrapbees)
 
+#Remove dates from bareground.bees without bare ground data
 bareground.bees <- bareground.bees %>%
   na.omit(avg.bareground)
 
@@ -95,11 +96,10 @@ bareground.etrapbees$Year <- year(bareground.etrapbees$Date)
 bareground.bees12 <- filter(bareground.bees, Year < 2016)
 bareground.etrapbees12 <- filter(bareground.etrapbees, Year < 2016)
 
-#Model for bee abundance predicted by bare ground without Year and Site.
-BGonBAmodel12 <- lmer(number.bees ~ avg.bareground + (1|Site) + (1|Year) + (1|Date),
+#Model for bee abundance predicted by bare ground.
+BGonBA12model <- lmer(number.bees ~ avg.bareground + (1|Site) + (1|Year) + (1|Date),
                     data = bareground.bees12)
-summary(BGonBAmodel12)
-coefficients(BGonBAmodel12)
+summary(BGonBA12model)
 
 #Change "Year" column to a factor.
 bareground.bees12$Year <- as.factor(bareground.bees12$Year)
@@ -113,6 +113,12 @@ BGonBA12plot <- ggplot(bareground.bees12,
              size = 3) +
   geom_abline(intercept = coef(summary(BGonBAmodel12))[ , "Estimate"][1],
               slope = coef(summary(BGonBAmodel12))[ , "Estimate"][2]) +
+  #geom_text(x = 10, y = 180,
+            #label = "y = 0.8216x + 49.4043",
+            #size = 5) +
+  #geom_text(x = 5, y = 170,
+            #label = "p = 0.037",
+            #size = 5) +
   theme_bw() +
   labs(x = "Bare Ground (%)",
        y = "Bee Abundance") +
@@ -120,7 +126,8 @@ BGonBA12plot <- ggplot(bareground.bees12,
   theme(plot.title = element_text(size = 15,
                                   face = "bold",
                                   hjust = 0.5)) +
-  theme(legend.text = element_text(size = 10))
+  theme(legend.text = element_text(size = 10)) +
+  theme(legend.title.align = 0.5)
 BGonBA12plot
 
 #Years 1-3 ####
@@ -128,47 +135,77 @@ BGonBA12plot
 #                            Years 1-3                              #
 #-------------------------------------------------------------------#
 #Subset data frames to include only 2014-2016 data.
-BGonBA123 <- filter(BGonBA, Year <= 3)
-BGonBA123condensed <- filter(BGonBAcondensed, Year <= 3)
+bareground.bees123 <- filter(bareground.bees, Year <= 2016)
 
-#Model for bee abundance predicted by bare ground including Year and Site as a random effects.
-BGYSonBA123model <- glmer(ETrapAbundance ~ AverageBareGround + (1|Date) + (1|Site),
-                          family = poisson,
-                          data = BGonBA123)
-summary(BGYSonBA123model)
-coef(BGYSonBA123model)
+#Model for bee abundance predicted by bare ground.
+BGonBA123model <- lmer(number.bees ~ avg.bareground + (1|Site) + (1|Year) + (1|Date),
+                       data = bareground.bees123)
+summary(BGonBA123model)
 
-#Model for emergence trap bee abundance predicted by bare ground using Year and Site as random effects.
-BGYSonBA123condensedmodel <- glmer(ETrapAbundance ~ AverageBareGround + (1|Year) + (1|Site),
-                                   family = poisson,
-                                   data = BGonBA123condensed)
-summary(BGYSonBA123condensedmodel)
-
-#Find intercept and slope to plot best fit line on graph
-coef(BGonBA123model)
-
-#Change "Year" column to factor.
-BGonBA123$Year <- as.factor(BGonBA123$Year)
+#Change "Year" column to a factor.
+bareground.bees123$Year <- as.factor(bareground.bees123$Year)
 
 #Morgan's plot: Percent Bare Ground vs. Bee Abundance plot using ggplot2
-BGonBA123plot <- ggplot(BGonBA123, aes(x = AverageBareGround,
-                                       y = ETrapAbundance)) +
+BGonBA123plot <- ggplot(bareground.bees123,
+                        aes(x = avg.bareground,
+                            y = number.bees)) +
   geom_point(aes(shape = Year,
                  color = Year),
              size = 3) +
-  geom_smooth(method = "glm",
-              se = FALSE,
-              color = "black",
-              size = 0.5) +
+  geom_abline(intercept = coef(summary(BGonBA123model))[ , "Estimate"][1],
+              slope = coef(summary(BGonBA123model))[ , "Estimate"][2]) +
+  #geom_text(x = 15, y = 500,
+            #label = "y = 0.7025x + 59.3321",
+            #size = 5) +
+  #geom_text(x = 10.25, y = 465,
+            #label = "p = 0.1907",
+            #size = 5) +
   theme_bw() +
-  labs(x = "Percent Bare Ground",
+  labs(x = "Bare Ground (%)",
        y = "Bee Abundance") +
-  ggtitle("Influence of Bare Ground on Bee Abundance") +
+  ggtitle("2014-2016\nInfluence of Bare Ground on Bee Abundance") +
   theme(plot.title = element_text(size = 15,
                                   face = "bold",
                                   hjust = 0.5)) +
-  theme(legend.text = element_text(size = 10))
+  theme(legend.text = element_text(size = 10)) +
+  theme(legend.title.align = 0.5)
 BGonBA123plot
+
+#Year 3 ####
+#-------------------------------------------------------------------#
+#                              Year 3                               #
+#-------------------------------------------------------------------#
+#Subset data frames to include only 2016 data.
+bareground.bees3 <- filter(bareground.bees, Year == 2016)
+
+#Model for bee abundance predicted by bare ground.
+BGonBA3model <- lmer(number.bees ~ avg.bareground + (1|Site) + (1|Date),
+                     data = bareground.bees3)
+summary(BGonBA3model)
+
+#Morgan's plot: Percent Bare Ground vs. Bee Abundance plot using ggplot2
+BGonBA3plot <- ggplot(bareground.bees3,
+                      aes(x = avg.bareground,
+                          y = number.bees)) +
+  geom_point(size = 3) +
+  geom_abline(intercept = coef(summary(BGonBA3model))[ , "Estimate"][1],
+              slope = coef(summary(BGonBA3model))[ , "Estimate"][2]) +
+  #geom_text(x = 10, y = 500,
+            #label = "y = 0.2569x + 78.5667",
+            #size = 5) +
+  #geom_text(x = 7.2, y = 465,
+            #label = "p = 0.9212",
+            #size = 5) +
+  theme_bw() +
+  labs(x = "Bare Ground (%)",
+       y = "Bee Abundance") +
+  ggtitle("2016\nInfluence of Bare Ground on Bee Abundance") +
+  theme(plot.title = element_text(size = 15,
+                                  face = "bold",
+                                  hjust = 0.5)) +
+  theme(legend.text = element_text(size = 10)) +
+  theme(legend.title.align = 0.5)
+BGonBA3plot
 
 #Years 3-4 ####
 #-------------------------------------------------------------------#
@@ -370,58 +407,66 @@ BGonBA1234plot
 
 
 #Years 4-5 ####
+#-------------------------------------------------------------------#
+#                           Years 4-5                               #
+#-------------------------------------------------------------------#
 #Subset bareground.bees and bareground.etrapbees to include only 2017 and 2018 data.
 bareground.bees45 <- filter(bareground.bees, Year > 2016)
 bareground.etrapbees45 <- filter(bareground.etrapbees, Year > 2016)
 
 #Model for bee abundance predicted by bare ground without Year and Site.
-BGonBAmodel45 <- lmer(number.bees ~ avg.bareground + (1|Site) + (1|Year) + (1|Date),
-                    data = bareground.bees45)
-summary(BGonBAmodel45)
-AIC(BGonBAmodel45)
+BGonBA45model <- lmer(number.bees ~ avg.bareground + (1|Site) + (1|Year) + (1|Date),
+                      data = bareground.bees45)
+summary(BGonBA45model)
+AIC(BGonBA45model)
 #AIC = 925.9541, p-value = 0.075522; SINGULAR FIT
 
-BGonBAmodel452 <- lmer(number.bees ~ avg.bareground + (1|Site) + (1|Year),
-                      data = bareground.bees45)
-summary(BGonBAmodel452)
-AIC(BGonBAmodel452)
+BGonBA45model2 <- lmer(number.bees ~ avg.bareground + (1|Site) + (1|Year),
+                       data = bareground.bees45)
+summary(BGonBA45model2)
+AIC(BGonBA45model2)
 #AIC = 923.9714, p-value = 0.076358; SINGULAR FIT
 
-BGonBAmodel453 <- lmer(number.bees ~ avg.bareground + (1|Site) + (1|Date),
-                      data = bareground.bees45)
-summary(BGonBAmodel453)
-AIC(BGonBAmodel453)
+BGonBA45model3 <- lmer(number.bees ~ avg.bareground + (1|Site) + (1|Date),
+                       data = bareground.bees45)
+summary(BGonBA45model3)
+AIC(BGonBA45model3)
 #AIC = 923.9541, p-value = 0.075522
 
-BGonBAmodel454 <- lmer(number.bees ~ avg.bareground + (1|Site),
-                      data = bareground.bees45)
-summary(BGonBAmodel454)
-AIC(BGonBAmodel454)
+BGonBA45model4 <- lmer(number.bees ~ avg.bareground + (1|Site),
+                       data = bareground.bees45)
+summary(BGonBA45model4)
+AIC(BGonBA45model4)
 #AIC = 921.9714, p-value = 0.076358
 
 #Change "Year" column to a factor.
-bareground.bees12$Year <- as.factor(bareground.bees12$Year)
+bareground.bees45$Year <- as.factor(bareground.bees45$Year)
 
 #Morgan's plot: Percent Bare Ground vs. Bee Abundance plot using ggplot2
-BGonBA12plot <- ggplot(bareground.bees12,
+BGonBA45plot <- ggplot(bareground.bees45,
                        aes(x = avg.bareground,
                            y = number.bees)) +
   geom_point(aes(shape = Year,
                  color = Year),
              size = 3) +
-  geom_smooth(method = "glm",
-              se = FALSE,
-              color = "black",
-              size = 0.5) +
+  geom_abline(intercept = coef(summary(BGonBA45model3))[ , "Estimate"][1],
+              slope = coef(summary(BGonBA45model3))[ , "Estimate"][2]) +
+  #geom_text(x = 16.2, y = 350,
+            #label = "y = 1.4433x + 86.1300",
+            #size = 5) +
+  #geom_text(x = 10.25, y = 325,
+            #label = "p = 0.07552",
+            #size = 5) +
   theme_bw() +
-  labs(x = "Percent Bare Ground",
+  labs(x = "Bare Ground (%)",
        y = "Bee Abundance") +
-  ggtitle("Influence of Bare Ground on Bee Abundance") +
+  ggtitle("2017-2018\nInfluence of Bare Ground on Bee Abundance") +
   theme(plot.title = element_text(size = 15,
                                   face = "bold",
                                   hjust = 0.5)) +
-  theme(legend.text = element_text(size = 10))
-BGonBA12plot
+  theme(legend.text = element_text(size = 10)) +
+  theme(legend.title.align = 0.5)
+BGonBA45plot
 
 #Years 1-5 ####
 #-------------------------------------------------------------------#
@@ -464,10 +509,14 @@ BGonBA12345plot <- ggplot(bareground.bees,
   geom_point(aes(shape = Year,
                  color = Year),
              size = 3) +
-  geom_smooth(method = "glm",
-              se = FALSE,
-              color = "black",
-              size = 0.5) +
+  geom_abline(intercept = coef(summary(BGonBA12345model3))[ , "Estimate"][1],
+              slope = coef(summary(BGonBA12345model3))[ , "Estimate"][2]) +
+  geom_text(x = 20, y = 500,
+            label = "y = 0.6873x + 73.6059",
+            size = 5) +
+  geom_text(x = 13, y = 465,
+            label = "p = 0.1010",
+            size = 5) +
   scale_color_manual(labels = c("2014", "2015", "2016", "2017", "2018"),
                      values = c("darkorchid1", "darkgreen", "#000000", "#FFB90F", "cornflowerblue")) +
   scale_shape_manual(labels = c("2014", "2015", "2016", "2017", "2018"),
@@ -541,6 +590,36 @@ BGonETBA12345plot <- ggplot(bareground.etrapbees,
   theme(legend.text = element_text(size = 10))
 BGonETBA12345plot
 
+#Multipanel graph showing bee abundance ~ bare ground for 2014-2015, 2016, and 2017-2018 together
+BGonBA12plot.grid <- BGonBA12plot +
+  expand_limits(y = c(0, 517)) +
+  expand_limits(x = c(0, 70)) +
+  geom_text(x = 20, y = 500,
+            label = "y = 0.8216x + 49.4043",
+            size = 5) +
+  geom_text(x = 10, y = 475,
+            label = "p = 0.037",
+            size = 5)
+BGonBA3plot.grid <- BGonBA3plot +
+  expand_limits(y = c(0, 517)) +
+  expand_limits(x = c(0, 70)) +
+  geom_text(x = 20, y = 500,
+            label = "y = 0.2569x + 78.5667",
+            size = 5) +
+  geom_text(x = 7.2, y = 475,
+            label = "p = 0.9212",
+            size = 5)
+BGonBA45plot.grid <- BGonBA45plot +
+  expand_limits(y = c(0, 517)) +
+  expand_limits(x = c(0, 70)) +
+  geom_text(x = 20, y = 500,
+            label = "y = 1.4433x + 86.1300",
+            size = 5) +
+  geom_text(x = 10, y = 475,
+            label = "p = 0.07552",
+            size = 5)
+grid.arrange(BGonBA12plot.grid, BGonBA3plot.grid, BGonBA45plot.grid, ncol = 3)
+
 #Data dictionary ####
 #Date = Date of sample
 #Site = Site name
@@ -595,3 +674,42 @@ legend("topleft",bty="n",
 model2=lm(BGonBA12$ETrapAbundance~0+BGonBA12$AverageBareGround)
 summary(model2)
 abline(model2, lty="dotted")
+
+#Model for bee abundance predicted by bare ground including Year and Site as a random effects.
+BGYSonBA123model <- glmer(ETrapAbundance ~ AverageBareGround + (1|Date) + (1|Site),
+                          family = poisson,
+                          data = BGonBA123)
+summary(BGYSonBA123model)
+coef(BGYSonBA123model)
+
+#Model for emergence trap bee abundance predicted by bare ground using Year and Site as random effects.
+BGYSonBA123condensedmodel <- glmer(ETrapAbundance ~ AverageBareGround + (1|Year) + (1|Site),
+                                   family = poisson,
+                                   data = BGonBA123condensed)
+summary(BGYSonBA123condensedmodel)
+
+#Find intercept and slope to plot best fit line on graph
+coef(BGonBA123model)
+
+#Change "Year" column to factor.
+BGonBA123$Year <- as.factor(BGonBA123$Year)
+
+#Morgan's plot: Percent Bare Ground vs. Bee Abundance plot using ggplot2
+BGonBA123plot <- ggplot(BGonBA123, aes(x = AverageBareGround,
+                                       y = ETrapAbundance)) +
+  geom_point(aes(shape = Year,
+                 color = Year),
+             size = 3) +
+  geom_smooth(method = "glm",
+              se = FALSE,
+              color = "black",
+              size = 0.5) +
+  theme_bw() +
+  labs(x = "Percent Bare Ground",
+       y = "Bee Abundance") +
+  ggtitle("Influence of Bare Ground on Bee Abundance") +
+  theme(plot.title = element_text(size = 15,
+                                  face = "bold",
+                                  hjust = 0.5)) +
+  theme(legend.text = element_text(size = 10))
+BGonBA123plot
